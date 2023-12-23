38a4f4073f53b7a3993b081f23fe7d1e4558306e
Ferry Manders <github@blackring.net>
2023-12-23 20:22:22 +0100

    more style fixing

--- src/server.go
+++ src/server.go
@@ -12,91 +12,91 @@ import (
 )
 
 func doQuery(w http.ResponseWriter, r *http.Request) {
-	// Fetch querier url from env
-	thanosQuerierUrl = os.Getenv("THANOS_QUERIER_URL")
+  // Fetch querier url from env
+  thanosQuerierUrl = os.Getenv("THANOS_QUERIER_URL")
 
-	// Get Authorization token to passthrough
-	incomingToken := strings.Split(r.Header.Get("Authorization"), " ")
-	authToken := incomingToken[1]
-	// Remove Authorization header to prevent it showing in logs
-	r.Header.Del("Authorization") 
+  // Get Authorization token to passthrough
+  incomingToken := strings.Split(r.Header.Get("Authorization"), " ")
+  authToken := incomingToken[1]
+  // Remove Authorization header to prevent it showing in logs
+  r.Header.Del("Authorization") 
 
-	// Get Query data to passthrough
-	query := r.URL.Query().Get("query")
-	namespace := r.URL.Query().Get("namespace")
+  // Get Query data to passthrough
+  query := r.URL.Query().Get("query")
+  namespace := r.URL.Query().Get("namespace")
 
-	// Debugging
-	fmt.Println("# New request ############################")
-	fmt.Println("GET params were:", r.URL.Query())
-	fmt.Println("Headers were:", r.Header)
+  // Debugging
+  fmt.Println("# New request ############################")
+  fmt.Println("GET params were:", r.URL.Query())
+  fmt.Println("Headers were:", r.Header)
 
-	// Start creating request to Querier
-	req, err := http.NewRequest("GET", thanosQuerierUrl, nil)
-	req.Header.Add("Authorization", "Bearer " + authToken)
+  // Start creating request to Querier
+  req, err := http.NewRequest("GET", thanosQuerierUrl, nil)
+  req.Header.Add("Authorization", "Bearer " + authToken)
 
-	// Add QueryString data to outgoing request
-	q := req.URL.Query()
-	q.Add("namespace", namespace)
-	q.Add("query", query)
-	req.URL.RawQuery = q.Encode()
+  // Add QueryString data to outgoing request
+  q := req.URL.Query()
+  q.Add("namespace", namespace)
+  q.Add("query", query)
+  req.URL.RawQuery = q.Encode()
 
-	// Send outgoing request
-	client := &http.Client{}
-	resp, err := client.Do(req)
+  // Send outgoing request
+  client := &http.Client{}
+  resp, err := client.Do(req)
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer resp.Body.Close()
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  defer resp.Body.Close()
 
-	// Pull json from response body
-	json, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
+  // Pull json from response body
+  json, err := io.ReadAll(resp.Body)
+  if err != nil {
+    log.Fatalln(err)
+  }
 
-	// Parse response json
-	result := gjson.Parse(string(json)).Get("data.result")
+  // Parse response json
+  result := gjson.Parse(string(json)).Get("data.result")
 
-	// Define which result keys to skip in output
-	skipTags := []string{"__name__", "job", "prometheus"}
+  // Define which result keys to skip in output
+  skipTags := []string{"__name__", "job", "prometheus"}
 
-	// Iterate through json metric objects
-	result.ForEach(func(key, value gjson.Result) bool {
-		jsonData := gjson.Parse(value.String())
+  // Iterate through json metric objects
+  result.ForEach(func(key, value gjson.Result) bool {
+    jsonData := gjson.Parse(value.String())
 
-		objectData := jsonData.Get("metric")
-		metricTime := jsonData.Get("value.0")
-		metricValue := jsonData.Get("value.1")
+    objectData := jsonData.Get("metric")
+    metricTime := jsonData.Get("value.0")
+    metricValue := jsonData.Get("value.1")
 
-		// Grab metric name
-		name := objectData.Get("__name__")
-		var tags string
-		objectData.ForEach(func(key, value gjson.Result) bool {
-			if !slices.Contains(skipTags, key.String()) {
-				tags = tags + key.String() + "=\"" + value.String() + "\","
-			}
-			return true
-		})
-		// Remove trailing comma from tags
-		tags = strings.TrimRight(tags, ",")
+    // Grab metric name
+    name := objectData.Get("__name__")
+    var tags string
+    objectData.ForEach(func(key, value gjson.Result) bool {
+      if !slices.Contains(skipTags, key.String()) {
+        tags = tags + key.String() + "=\"" + value.String() + "\","
+      }
+      return true
+    })
+    // Remove trailing comma from tags
+    tags = strings.TrimRight(tags, ",")
 
-		// output metrics in OpenTelemetry style
-		fmt.Fprintf(w, "%s{%s} %s %s\n", name, tags, metricTime.String(), metricValue.String())
+    // output metrics in OpenTelemetry style
+    fmt.Fprintf(w, "%s{%s} %s %s\n", name, tags, metricTime.String(), metricValue.String())
 
-		return true 
-	})
+    return true 
+  })
 }
 
 func main() {
-	// Create new webserver
+  // Create new webserver
   mux := http.NewServeMux()
 
-	// Define routes
+  // Define routes
   mux.HandleFunc("/query", doQuery)
 
-	// Start listening
+  // Start listening
   err := http.ListenAndServe(":4000", mux)
   log.Fatal(err)
 }
